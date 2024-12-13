7913da2de4cb01a7b84a98434e1da30cf03f57c4
gavinconran <gtconran@gmail.com>
2024-12-13 16:04:04 +0000

    SymmetricGroup

---
+++ groups/symmetric_groups.py
@@ -0,0 +1,28 @@
+"""A module implementing the basic functionality of mathematical groups.
+
+This version of the module uses inheritance.
+"""
+
+from numbers import Integral
+import numpy as np
+import example_code.groups.Group
+
+
+class SymmetricGroup(Group):
+    """A symmetric group over n values."""
+
+    symbol = "S"
+
+    def _validate(self, value):
+        """Ensure that value is an allowed element value in this group."""
+        value = np.ndarray(value) # sort
+        if not (len(value) == self.n and sorted(value) == [i for i in np.arange(self.n)]): 
+            raise ValueError("Element value must be an integer"
+                             f" in the range [0, {self.n})")
+
+    def operation(self, a, b):
+        """Perform the group operation on two values.
+
+        The group operation is  the composition a(b).
+        """
+        return a[b]
