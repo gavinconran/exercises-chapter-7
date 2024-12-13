a98b50ea8da63d7c9268ba8c334298791cd5ea5f
gavinconran <gtconran@gmail.com>
2024-12-13 17:18:54 +0000

    groups __init__ fix

--- example_code/__init__.py
+++ example_code/__init__.py
@@ -1,3 +1,2 @@
 """Contains the groups __init__ file Chp  of OOP4Maths."""
-from groups.groups import CyclicGroup, GeneralLinearGroup # noqa F401
 from groups.groups import Group, Element # noqa F401
