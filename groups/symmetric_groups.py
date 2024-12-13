196c6a869ad988cddea3f940e83598e216e65e14
gavinconran <gtconran@gmail.com>
2024-12-13 17:29:01 +0000

    _validate string_ fix

--- groups/symmetric_groups.py
+++ groups/symmetric_groups.py
@@ -14,11 +14,14 @@ class SymmetricGroup(Group):
     symbol = "S"
 
     def _validate(self, value):
-        """Ensure that value is an allowed element value in this group."""
-        value = np.ndarray(value)
-        if not (sorted(value.shape) == [i for i in np.arange(self.n)]): 
-            raise ValueError("Element value must be a permutation of integers"
-                             f" in the range [0, {self.n})")
+        """Ensure that value is an allowed element value in this group."""                       
+        if (isinstance(value, str)):
+            raise ValueError("Element value must be a permutation of"
+                             f"integers in the range [0, {self.n})")
+        if not (sorted(np.ndarray(value).shape) == 
+                [i for i in np.arange(self.n)]): 
+                raise ValueError("Element value must be a permutation of"
+                             f"integers in the range [0, {self.n})")
 
     def operation(self, a, b):
         """Perform the group operation on two values.
