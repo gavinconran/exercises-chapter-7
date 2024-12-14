c416bf73c3093113c1a5939c7cb919a87833ca29
gavinconran <gtconran@gmail.com>
2024-12-14 16:32:53 +0000

    relative import in groups module __init__.py

--- example_code/__init__.py
+++ example_code/__init__.py
@@ -1,2 +1,3 @@
 """Contains the groups __init__ file Chp  of OOP4Maths."""
-from example_code.groups import Group, Element # noqa F401
+# from example_code.groups import Group, Element # noqa F401
+from .groups import Group, Element # noqa F401
