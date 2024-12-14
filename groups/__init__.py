e3df633b1f711e49dd568374b5090a3230ef8fcd
gavinconran <gtconran@gmail.com>
2024-12-14 16:35:24 +0000

    relative import in symmetric_groups module __init__.py

--- groups/__init__.py
+++ groups/__init__.py
@@ -1,2 +1,3 @@
 """Contains the groups __init__ file Chp  of OOP4Maths."""
-from groups.symmetric_groups import SymmetricGroup # noqa F401
+# from groups.symmetric_groups import SymmetricGroup # noqa F401
+from .symmetric_groups import SymmetricGroup # noqa F401
